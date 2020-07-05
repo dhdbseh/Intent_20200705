967ccdb5ef335e8795aec6497d7674f12341fbf1
yd27 <dhdbseh5oe@naver.com>
2020-07-05 13:48:23 +0900

    닉네임을 변경하기 위한 화면 이동 구현 -입력 완료 처리 구현 필요

--- app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
@@ -34,5 +34,22 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(myIntent)
         }
+
+        //닉네임 변경 버튼을 누르면
+        changeNickBtn.setOnClickListener{
+
+            //닉네임 변경 화면으로 가야함.Intent
+
+            val myIntent = Intent(this, EditNickNameActivity::class.java)
+
+            //화면으로 이동 -> 단순 조회 이동 X
+            //데이터 받으러 (결과를 얻으러) 가는 이동 -> 뭘 받으러 가는지 명시.
+            //받을 데이터 (요청) 구별 고유 숫자 : requestCode
+
+            //어떤 결과? -> 새 닉네임을 얻으러.(1000 : 새 닉네임)
+            startActivityForResult(myIntent, 1000)
+        }
+
+
     }
 }
