2b7d71ec5929455de9a7433a6a23f8be6a7f17c0
yd27 <dhdbseh5oe@naver.com>
2020-07-05 12:19:21 +0900

    첫번째 액티비티로 이동하기

--- app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
@@ -2,10 +2,19 @@ package kr.co.tjoeun.intent_20200705
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToFirstBtn.setOnClickListener {
+            
+            //비행기 티켓 발권 (출발 / 도착)
+            val myIntent = Intent(this, FirstActivity::class.java)
+            //실제로 출발하기
+            startActivity(myIntent)
+         }
     }
 }
